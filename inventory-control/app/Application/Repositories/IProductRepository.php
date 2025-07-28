4456ea040d2fef3fbd3ae63057ea3dcf00c64b74
DEXS <contato.dexs@gmail.com>
2025-07-28 19:32:11 -0300

    feat: make IProduct repository

---
+++ inventory-control/app/Application/Repositories/IProductRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+// app/Application/Repositories/IProductRepository.php
+namespace App\Application\Repositories;
+
+use App\Domain\Entities\Product;
+
+interface IProductRepository
+{
+    public function save(Product $product): void;
+    public function findAll(): array;
+    public function findById(int $id): Product;
+    public function delete(int $id): void;
+}
