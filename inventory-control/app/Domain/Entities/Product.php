bda28a3a7c5fdc7910c278d5919de8612f1e2010
DEXS <contato.dexs@gmail.com>
2025-07-28 19:33:52 -0300

    refactor: change path to Domain/Entities and enrich model

---
+++ inventory-control/app/Domain/Entities/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+// app/Domain/Entities/Product.php
+namespace App\Domain\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'active',
+    ];
+}
