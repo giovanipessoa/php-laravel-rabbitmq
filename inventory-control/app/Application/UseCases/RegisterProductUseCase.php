a9e5b3c751915cc957d93e81cbb12467fb9ac00a
DEXS <contato.dexs@gmail.com>
2025-08-04 16:38:58 -0300

    feat: add use case to register product

---
+++ inventory-control/app/Application/UseCases/RegisterProductUseCase.php
@@ -0,0 +1,31 @@
+<?php
+
+// app/Application/UseCases/RegisterProductUseCase.php
+namespace App\Application\UseCases;
+
+use App\Application\Repositories\IProductRepository;
+use App\Domain\Entities\Product;
+use InvalidArgumentException;
+
+class RegisterProductUseCase
+{
+    public function __construct(private IProductRepository $productRepository) {}
+
+    public function execute(array $productData): Product
+    {
+        try {
+            $product = new Product([
+                'name' => $productData['name'],
+                'description' => $productData['description'],
+                'price' => $productData['price'],
+                'stock' => $productData['stock'],
+                'active' => $productData['active']
+            ]);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+
+        $this->productRepository->save($product);
+        return $product;
+    }
+}
