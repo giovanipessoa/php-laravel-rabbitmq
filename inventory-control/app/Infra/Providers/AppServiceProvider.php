754d1ae452f97d932c6e4a37c6e5032a3970826d
DEXS <contato.dexs@gmail.com>
2025-08-04 16:46:13 -0300

    feat: bind interface to its concrete implementation

--- inventory-control/app/Infra/Providers/AppServiceProvider.php
+++ inventory-control/app/Infra/Providers/AppServiceProvider.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace App\Providers;
+// app/Infra/Providers/AppServiceProvider.php
+namespace App\Infra\Providers;
 
+use App\Application\Repositories\IProductRepository;
+use App\Infra\Repositories\EProductRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // bind interface to its concrete implementation
+        $this->app->bind(IProductRepository::class, EProductRepository::class);
     }
 
     /**
