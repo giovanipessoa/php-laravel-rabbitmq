d368e266d75b192fbc33d24fb4b8a609394fa11b
DEXS <contato.dexs@gmail.com>
2025-08-04 16:37:50 -0300

    refactor: enrich model
    - set attributes that can be include in mass.
    - validate the domain to ensure consistency, because business rules must always be true.
    - add behavior methods to the domain.

--- inventory-control/app/Domain/Entities/Product.php
+++ inventory-control/app/Domain/Entities/Product.php
@@ -5,10 +5,13 @@ namespace App\Domain\Entities;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
 
 class Product extends Model
 {
     use HasFactory;
+
+    // fillable to specify the fields that can be mass assigned
 
     protected $fillable = [
         'name',
@@ -18,7 +21,26 @@ class Product extends Model
         'active',
     ];
 
-    // methods for domain behavior
+    // explicit constructor to ensure validation
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (empty($this->name)) {
+            throw new InvalidArgumentException("Name is required");
+        }
+
+        if ($this->price < 0) {
+            throw new InvalidArgumentException("Price must be greater than 0");
+        }
+
+        if ($this->stock < 0) {
+            throw new InvalidArgumentException("Stock must be greater than 0");
+        }
+    }
+
+    // behavior methods to the domain
 
     public function increaseStock(int $quantity): void
     {
@@ -28,7 +50,7 @@ class Product extends Model
     public function decreaseStock(int $quantity): void
     {
         if ($this->stock < $quantity) {
-            throw new \InvalidArgumentException('Stock is not enough');
+            throw new InvalidArgumentException('Stock is not enough');
         }
 
         $this->stock -= $quantity;
