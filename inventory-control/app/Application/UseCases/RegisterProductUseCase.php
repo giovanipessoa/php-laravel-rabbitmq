c34291c770f488ebed1f99f8548751ddcf0cbcef
DEXS <contato.dexs@gmail.com>
2025-08-06 14:53:31 -0300

    feat: add validation

--- inventory-control/app/Application/UseCases/RegisterProductUseCase.php
+++ inventory-control/app/Application/UseCases/RegisterProductUseCase.php
@@ -14,12 +14,24 @@ class RegisterProductUseCase
     public function execute(array $productData): Product
     {
         try {
+            if (empty($productData['name'])) {
+                throw new InvalidArgumentException("Name is required");
+            }
+
+            if ($productData['price'] < 0) {
+                throw new InvalidArgumentException("Price must be greater than 0");
+            }
+
+            if ($productData['stock'] < 0) {
+                throw new InvalidArgumentException("Stock must be greater than 0");
+            }
+
             $product = new Product([
                 'name' => $productData['name'],
                 'description' => $productData['description'],
                 'price' => $productData['price'],
                 'stock' => $productData['stock'],
-                'active' => $productData['active']
+                'active' => true
             ]);
         } catch (InvalidArgumentException $e) {
             throw new InvalidArgumentException($e->getMessage());
