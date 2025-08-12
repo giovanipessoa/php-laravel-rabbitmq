5919773a73a8bdc83b0c8419a2b30ad3c9878641
DEXS <contato.dexs@gmail.com>
2025-08-12 20:37:28 -0300

    chore: removes validation

--- inventory-control/src/Application/UseCases/RegisterProductUseCase.php
+++ inventory-control/src/Application/UseCases/RegisterProductUseCase.php
@@ -4,40 +4,24 @@
 namespace AppCore\Application\UseCases;
 
 use AppCore\Application\Repositories\InterfaceProductRepository;
+use AppCore\Application\DTO\RegisterProductData;
 use AppCore\Domain\Entities\Product;
-use InvalidArgumentException;
 
 class RegisterProductUseCase
 {
     public function __construct(private InterfaceProductRepository $productRepository) {}
 
-    public function execute(array $productData): Product
+    public function execute(RegisterProductData $data): Product
     {
-        try {
-            if (empty($productData['name'])) {
-                throw new InvalidArgumentException("Name is required");
-            }
+        $product = new Product(
+            $data->name,
+            $data->description,
+            $data->price,
+            $data->stock,
+            true
+        );
 
-            if ($productData['price'] < 0) {
-                throw new InvalidArgumentException("Price must be greater than 0");
-            }
-
-            if ($productData['stock'] < 0) {
-                throw new InvalidArgumentException("Stock must be greater than 0");
-            }
-
-            $product = new Product(
-                $productData['name'],
-                $productData['price'],
-                $productData['stock'],
-                $productData['description'],
-                true
-            );
-
-            $this->productRepository->save($product);
-            return $product;
-        } catch (InvalidArgumentException $e) {
-            throw new InvalidArgumentException($e->getMessage());
-        }
+        $this->productRepository->save($product);
+        return $product;
     }
 }
